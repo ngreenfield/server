91efba09c2d0c1683d26656aa0f6fb2659e354a7
ngreenfield <nagreenfield@yahoo.com>
2025-01-30 16:22:39 -0800

    assignment 3

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import json
+from http import HTTPStatus
 
 app = Flask(__name__)
 
@@ -20,6 +21,45 @@ def test():
 def about():
     name={"name":"Nick"}
     return json.dumps(name)
+
+products = []
+
+#GET all products
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products), HTTPStatus.OK
+
+#POST a product
+@app.post("/api/products")
+def save_product():
+    product = request.get_json()
+    print(f"product {product}")
+    products.append(product)
+    return "Product saved", 201
+
+#PUT a product
+@app.put("/api/products/<int>index>")
+def update_product(index):
+    updated_product = request.get_json()
+    print (f"update the product with index {index}")
+
+    if 0 <= index < len(products):
+        products[index] = updated_product
+        return json.dumps(updated_product), 200
+    else:
+        return "That index does not exist", 404
+
+#DELETE a product
+@app.delete("/api/products/<int:index>")
+def delete_product(index):
+    print(f"delete the product with index {index}")
+
+    if index >=0 and index < len(products):
+        deleted_product = products.pop(index)
+        return json.dumps(deleted_product)
+    else:
+        return "That index does not exist", HTTPStatus.NOT_FOUND
+
 
 app.run(debug=True)
 
